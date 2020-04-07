2663938e1740215505d0379d7aa9461d518e89c8
benLbishop <ben.bishop374@gmail.com>
2020-04-07 12:41:07 -0700

    Reorganized the components.

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../res/images/logo.svg';
+import './App.scss';
+import SoundTest from '../SoundTest';
+
+function App() {
+  return (
+    <div id="appInitialized">
+      <SoundTest />
+    </div>
+  );
+}
+
+export default App;
--- src/components/SoundTest/index.tsx
+++ src/components/SoundTest/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Howl } from 'howler';
-import j from '../res/sounds/highclick.wav';
+import highClick from '../../res/sounds/highclick.wav';
+import './SoundTest.scss';
 
 interface Props {}
 
 const sound = new Howl({
-  src: [j],
+  src: [highClick],
   onloaderror: (id, err) => {
     console.log('HOWL err: ', id, err)
   },
@@ -20,7 +21,7 @@ class SoundTest extends React.PureComponent<Props> {
 
   render() {
     return (      
-      <div>
+      <div id="soundTestContainer">
         <button onMouseDown={this.playSound}>Sound Test</button>
       </div>
     )
