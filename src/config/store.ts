d787a6294331799714b930f84dbeaf49702e79d8
benLbishop <ben.bishop374@gmail.com>
2020-04-15 19:14:12 -0700

    Started working with redux.

---
+++ src/config/store.ts
@@ -0,0 +1,34 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { LoggerPredicate, createLogger } from 'redux-logger';
+
+import { RootState } from '../reducers';
+
+const rootReducer = combineReducers({
+
+});
+
+const logActionFilter: LoggerPredicate = (getState: () => RootState, action: any) => {
+    return true;
+};
+
+const logStateFilter = (state: RootState): Partial<RootState> => {
+    return state;
+};
+
+const logger = createLogger({
+    predicate: logActionFilter,
+    stateTransformer: logStateFilter
+});
+
+const configureStore = () => {
+    const store = createStore(
+        rootReducer,
+        process.env.NODE_ENV !== 'production' ? //don't apply logger middleware in production
+            applyMiddleware(ReduxThunk, logger) :
+            applyMiddleware(ReduxThunk)
+    );
+    return { store };
+};
+
+export default configureStore;
